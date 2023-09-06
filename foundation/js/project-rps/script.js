270608b48760ef50c7749c1e8477ca80747c4644
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-06 13:44:17 +0700

    Add random choice function
    for the computer selection

--- foundation/js/project-rps/script.js
+++ foundation/js/project-rps/script.js
@@ -3,16 +3,16 @@ const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 
 // Function to generate the computer selection in a random basis
-// function getComputerChoice() {
-//     let choice = Math.floor(Math.random() * 10) + 1;
-//     if (choice >= 1 && choice <= 4) {
-//         return "rock";
-//     } else if (choice >= 5 && choice <= 7) {
-//         return "paper";
-//     } else {
-//         return "scissors";
-//     }
-// }
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 10) + 1;
+    if (choice >= 1 && choice <= 4) {
+        return "rock";
+    } else if (choice >= 5 && choice <= 7) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
 // Store the result to the computer selection variable
 // Function that takes two parameter, the computer selection and the player selection
 // Turn both the arguments to all upper case
