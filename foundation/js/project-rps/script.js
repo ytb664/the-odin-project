d8e5e900a8ca2b2fe87c966d06321b706717330c
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-06 13:57:29 +0700

    Finish playRound function
    finished the game mechanic

--- foundation/js/project-rps/script.js
+++ foundation/js/project-rps/script.js
@@ -1,6 +1,7 @@
 // Create variables to hold computer selection and the player selection
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 // Function to generate the computer selection in a random basis
 function getComputerChoice() {
@@ -20,5 +21,19 @@ function playRound(playerSelection, computerSelection) {
     // Turn both the arguments to all upper case
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
+
+    // Comparing both selection and return output accordingly
+    if (playerSelection === computerSelection) {
+        return `DRAW! BOTH SELECTS ${playerSelection}`;
+    }
+    if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return `YOU WIN! ${playerSelection} BEATS ${computerSelection}`;
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return `YOU WIN! ${playerSelection} BEATS ${computerSelection}`;
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return `YOU WIN! ${playerSelection} BEATS ${computerSelection}`;
+    } else {
+        return `YOU LOSE! ${computerSelection} BEATS ${playerSelection}`;
+    }
 }
 // Return a string based on the result from the previous function
