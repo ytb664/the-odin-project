53f00c5464aeec02b4886c8c37693f60dea00416
ytb <62708858+ytb664@users.noreply.github.com>
2023-09-13 03:54:45 +0700

    Add blue h3 text

--- foundation/js/dom/script.js
+++ foundation/js/dom/script.js
@@ -9,5 +9,11 @@ para.classList.add('para');
 para.textContent = 'Hey I\'m red!';
 para.style.color = 'red';
 
+const h3 = document.createElement('h3');
+h3.classList.add('h3');
+h3.textContent = "I\'m a blue h3!";
+h3.style.color = 'blue';
+
 container.appendChild(content);
 container.appendChild(para);
+container.appendChild(h3);
