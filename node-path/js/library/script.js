9bc6e99626cd90fc491807fc87ed96b045d57982
ytb <62708858+ytb664@users.noreply.github.com>
2023-12-06 08:38:51 +0700

    Restart code

--- node-path/js/library/script.js
+++ node-path/js/library/script.js
@@ -1,7 +1,13 @@
 const myLibrary = [];
 
-function Book() {
-    
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = isRead ? 'read' : 'not read yet';
+    this.info = function() {
+        return `${this.name} by ${this.author}, ${this.pages} pages, ${this.read}`
+    };
 }
 
 function addBookToLibrary() {
