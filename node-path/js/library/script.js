65eda4edf05f89e94eabe37b954e31ead12cdc43
ytb <62708858+ytb664@users.noreply.github.com>
2023-12-08 00:09:27 +0700

    Add display library

--- node-path/js/library/script.js
+++ node-path/js/library/script.js
@@ -5,11 +5,31 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.read = isRead ? 'read' : 'not read yet';
-    this.info = function() {
-        return `${this.name} by ${this.author}, ${this.pages} pages, ${this.read}`
-    };
 }
 
-function addBookToLibrary() {
+Book.prototype.info = () => {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+}
 
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+const container = document.querySelector('.container');
+
+function displayBooksInLibrary() {
+    myLibrary.forEach((el) => {
+        const card = document.createElement('div');
+        const ul = document.createElement('ul');
+
+        container.appendChild(card);
+        card.appendChild(ul);
+
+        for (let x in el) {
+            const li = document.createElement('li');
+            li.textContent = `${x} : ${el[x]}`;
+
+            ul.appendChild(li);
+        }
+    })
 }
