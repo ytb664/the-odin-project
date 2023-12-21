673a2b949274ee18aa32b6fa68776508f7b518d0
ytb <62708858+ytb664@users.noreply.github.com>
2023-12-21 17:53:29 +0700

    Fix file bug
    merge 2 js files into one

--- node-path/js/tic-tac-toe/js/script.js
+++ node-path/js/tic-tac-toe/js/script.js
@@ -123,4 +123,52 @@ const createMatch = (playerX, playerO, gameBoard) => {
     };
 }
 
-export { createGameBoard, createPlayer, createMatch };
+// Access objects and functions
+const gameBoard = createGameBoard();
+const playerX = createPlayer("Player X", "X");
+const playerO = createPlayer("Player O", "O");
+const match = createMatch(playerX, playerO, gameBoard);
+
+(() => {
+
+    // Appends elements to a parent
+    const appendElements = (parent, ...elements) => {
+
+        elements.forEach((element) => {
+            parent.appendChild(element);
+        });
+    };
+
+    // Creates an element with a specified tag and class
+    const createElementWithClass = (tagName, className) => {
+
+        const element = document.createElement(tagName);
+        element.classList.add(className);
+
+        return element;
+    };
+
+    // Appends an element with a specified tag and class to a parent
+    const appendElementWithClass = (parent, tagName, className) => {
+
+        const element = createElementWithClass(tagName, className);
+        parent.appendChild(element);
+
+        return element;
+    };
+
+    // Initializes UI elements
+    const initializeUI = (() => {
+
+        const container = document.querySelector('.container');
+        const header = appendElementWithClass(container, 'header', 'header');
+        const main = appendElementWithClass(container, 'main', 'main');
+        const footer = appendElementWithClass(container, 'footer', 'footer');
+
+        const turn = appendElementWithClass(main, 'div', 'turn');
+        const board = appendElementWithClass(main, 'div', 'board');
+        const btn = appendElementWithClass(main, 'button', 'reset');
+
+        return { container, header, main, footer, turn, board, btn };
+    })();
+})();
