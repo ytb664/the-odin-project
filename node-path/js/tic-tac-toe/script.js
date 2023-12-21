8bb05c5fe931f84e03ac02f27334376aeb44613b
ytb <62708858+ytb664@users.noreply.github.com>
2023-12-21 15:23:40 +0700

    Create game logic

--- node-path/js/tic-tac-toe/script.js
+++ node-path/js/tic-tac-toe/script.js
@@ -1,5 +1,5 @@
 // Game board factory
-const createGameBoard = (() => {
+const createGameBoard = () => {
 
     const board = Array(9).fill(null);
 
@@ -26,7 +26,7 @@ const createGameBoard = (() => {
     }
 
     return { getBoard, markCell, resetBoard };
-})();
+};
 
 // Player factory
 const createPlayer = (name, marker) => {
@@ -39,3 +39,92 @@ const createPlayer = (name, marker) => {
 
     return { getName, getMarker };
 }
+
+// Match factory
+const createMatch = (playerX, playerO, gameBoard) => {
+
+    let currentPlayer = playerX;
+
+    const switchPlayer = () => {
+
+        currentPlayer = currentPlayer === playerX ? playerO : playerX;
+    };
+
+    const mark = (index) => {
+
+        if (checkWin()) {
+
+            return false;
+        }
+
+        if (gameBoard.markCell(index, currentPlayer.getMarker())) {
+
+            switchPlayer();
+            return true;
+        }
+
+        return false;
+    };
+
+    const checkWin = () => {
+
+        const board = gameBoard.getBoard();
+
+        const winningCombination = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],    // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],    // Columns
+            [0, 4, 8], [2, 4, 6]                // Diagonals
+        ];
+
+        for (const combo of winningCombination) {
+
+            const [a, b, c] = combo;
+
+            const isNotNull = board[a] !== null;
+            const isSameMark = board[a] === board[b] && board[a] === board[c];
+
+            // Check if the markers at positions a, b, and c are the same and not null
+            if (isNotNull && isSameMark) {
+
+                return board[a]; // We have a winner
+            }
+        }
+
+        return null;
+    };
+
+    const checkDraw = () => {
+
+        const board = gameBoard.getBoard();
+
+        // Check for a win first
+        if (checkWin()) {
+            
+            return false;
+        }
+
+        return !board.includes(null);
+    };
+
+    const resetMatch = () => {
+
+        gameBoard.resetBoard();
+        currentPlayer = playerX;
+    };
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    return {
+        mark,
+        checkWin,
+        checkDraw,
+        resetMatch,
+        getCurrentPlayer
+    };
+}
+
+const gameBoard = createGameBoard();
+const playerX = createPlayer("Player X", "X");
+const playerO = createPlayer("Player O", "O");
+
+const match = createMatch(playerX, playerO, gameBoard);
